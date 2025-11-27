9fba2e9613708f97d0b6d01797962346b4c45068
leider <derleider@web.de>
2025-11-27 16:10:11 +0100

    disable more kinds of automails (part 2)

--- application/backend/batches/batchendpoints.js
+++ application/backend/batches/batchendpoints.js
@@ -33,14 +33,14 @@ async function nightlyMails() {
         //checkMaster(now),
     ]);
     const jobtypes = [
-        "Presse",
-        "Fluegel",
-        "Photo",
+        //"Presse",
+        //"Fluegel",
+        //"Photo",
         "TextFehlt",
         //"Kasse",
         "Programmheft",
         //"Staff",
-        "Bar",
+        //"Bar",
         //"Master"
     ];
     const typedResults = map(results, (jobResult, index) => ({ type: jobtypes[index], jobResult }));
